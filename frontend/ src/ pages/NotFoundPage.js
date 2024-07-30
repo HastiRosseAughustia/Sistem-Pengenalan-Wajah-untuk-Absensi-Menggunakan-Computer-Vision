52f31577836c83b1a8f73553ad8b75182c1f0535
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 19:29:33 +0800

    Update NotFoundPage.js

--- frontend/ src/ pages/NotFoundPage.js
+++ frontend/ src/ pages/NotFoundPage.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFoundPage.css'; // Mengimpor file CSS untuk styling
+
+const NotFoundPage = () => {
+    return (
+        <div className="not-found-page">
+            <h1>404 - Page Not Found</h1>
+            <p>Oops! The page you're looking for does not exist.</p>
+            <Link to="/" className="back-home-button">Go to Home Page</Link>
+        </div>
+    );
+};
+
+export default NotFoundPage;
 
