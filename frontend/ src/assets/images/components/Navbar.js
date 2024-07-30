911b3dcc9652dfa6a2446fdd19d7acde738eb31f
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 19:34:15 +0800

    Update Navbar.js

--- frontend/ src/assets/images/components/Navbar.js
+++ frontend/ src/assets/images/components/Navbar.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Mengimpor file CSS untuk styling
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <h1>MyApp</h1>
+            </div>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/attendance" className="nav-link">Attendance</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/capture" className="nav-link">Face Capture</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/users" className="nav-link">Users</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
 
