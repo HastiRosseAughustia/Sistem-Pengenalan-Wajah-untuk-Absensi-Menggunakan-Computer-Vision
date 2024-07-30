a6a98afcde48562562d5cc53e8fc5e4a2c31b67e
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 19:28:30 +0800

    Update HomePage.js

--- frontend/ src/ pages/HomePage.js
+++ frontend/ src/ pages/HomePage.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './HomePage.css'; // Mengimpor file CSS untuk styling
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Welcome to the Attendance and Face Recognition System</h1>
+            <p>This is the home page of our application where you can navigate to different sections.</p>
+            <div className="links">
+                <Link to="/attendance" className="link-button">Go to Attendance Page</Link>
+                <Link to="/capture" className="link-button">Go to Capture Page</Link>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
 
