42a0a90acf48e1b00f9842b96022d7fa300f040b
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 18:47:24 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/attendanceDB', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
 
