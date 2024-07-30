3a06c254f3a39c7425caa69695778eb3f4b5b16b
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 18:50:43 +0800

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,1 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb://localhost:27017/attendanceDB', // Ganti dengan URI MongoDB Anda
+    jwtSecret: 'your_jwt_secret', // Ganti dengan kunci rahasia JWT Anda
+    jwtExpire: '1h' // Atur waktu kedaluwarsa token JWT
+};
 
