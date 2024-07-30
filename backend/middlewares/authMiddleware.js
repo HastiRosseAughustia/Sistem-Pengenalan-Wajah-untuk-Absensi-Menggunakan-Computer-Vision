eb83c4b52c19f4f5e547a9bc74b5cdc72f1ebe68
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 18:53:35 +0800

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys'); // Menyesuaikan dengan lokasi file keys.js
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+        return res.status(401).send({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, keys.jwtSecret);
+        req.user = decoded; // Menyimpan informasi pengguna yang terdecode ke dalam request object
+        next(); // Melanjutkan ke middleware berikutnya atau rute yang diminta
+    } catch (error) {
+        res.status(400).send({ message: 'Invalid token.' });
+    }
+};
+
+module.exports = authMiddleware;
 
