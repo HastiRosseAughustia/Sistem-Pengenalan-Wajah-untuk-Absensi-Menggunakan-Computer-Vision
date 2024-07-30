a7d8b652cd26367e70b5d34de03283f6606bb366
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 19:52:54 +0800

    Update index.js

--- frontend/ src/index.js
+++ frontend/ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store'; // Impor store Redux
+import App from './App'; // Impor komponen utama aplikasi
+import './styles/main.css'; // Impor file CSS utama
+
+ReactDOM.render(
+    <Provider store={store}> {/* Membungkus aplikasi dengan Provider */}
+        <App /> {/* Render aplikasi utama */}
+    </Provider>,
+    document.getElementById('root') // Elemen DOM tempat aplikasi akan dirender
+);
 
