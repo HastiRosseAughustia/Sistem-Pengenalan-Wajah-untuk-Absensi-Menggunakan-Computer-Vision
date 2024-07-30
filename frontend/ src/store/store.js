dee1424037ebc5513ffdd2055d91f9f72230fb20
HastiRosseAughustia <hastiaughustia@gmail.com>
2024-07-30 19:48:43 +0800

    Update store.js

--- frontend/ src/store/store.js
+++ frontend/ src/store/store.js
@@ -1,1 +1,25 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import attendanceReducer from './reducers/attendanceReducer';
+import faceReducer from './reducers/faceReducer';
+import userReducer from './reducers/userReducer';
+
+// Menggabungkan semua reducer
+const rootReducer = combineReducers({
+    attendance: attendanceReducer,
+    face: faceReducer,
+    user: userReducer
+});
+
+// Konfigurasi middleware
+const middleware = [thunk];
+
+// Membuat store Redux
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
 
